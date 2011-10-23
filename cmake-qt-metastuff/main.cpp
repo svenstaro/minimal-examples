0fbc597151fa558c86032c44924e6d073af61b61
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-10-23 21:48:17 +0200

    Output id

--- cmake-qt-metastuff/main.cpp
+++ cmake-qt-metastuff/main.cpp
@@ -2,11 +2,14 @@
 
 #include <QMetaType>
 
+#include <iostream>
+
 int main() {
     Node testnode1;
     testnode1.DoStuff();
 
     int id = QMetaType::type("Node");
+    std::cout << "id: " << id << std::endl;
     if(id != -1) {
         Node* testnode2 = static_cast<Node*>(QMetaType::construct(id));
         testnode2->DoStuff();
